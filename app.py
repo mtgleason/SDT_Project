99d77805196d89121e5b06f932bd5379a94220c4
Michael Gleason <gleason.michaelt@gmail.com>
2024-04-23 23:10:37 -0500

    final

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import plotly.express as px
 df = pd.read_csv('vehicles_us_fixed.csv')
 
 st.title('SDT Project')
-st.write('This project is unfinished')
+st.write('This web app compares statistics from a car ad hosting site')
 
 year_1 = st.checkbox('Cars posted in 2018', value=True)
 year_2 = st.checkbox('Cars posted in 2019', value=True)
@@ -13,9 +13,9 @@ year_2 = st.checkbox('Cars posted in 2019', value=True)
 
 
 if year_1:
-    df['date_posted_year'] == 2018
+    df = df[df['date_posted_year'] == 2018]
 if year_2:
-    df['date_posted_year'] == 2019
+    df = df[df['date_posted_year'] == 2019]
 
 odometer_condition = px.scatter(df, x='vehicle_age', y='odometer', color='condition', opacity=.7)
 manufacturer_stats = px.histogram(df, x='type', color='manufacturer')
